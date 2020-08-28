b390286b6a2b881a6bb4baf08cee5add3a6c37f2
Gober <reyesfragosoroberto@gmail.com>
2020-08-28 03:51:56 +0000

    Move all functions to different files

---
+++ resume_checker/network.py
@@ -0,0 +1,14 @@
+from typing import List
+
+import requests
+
+
+def _url_checker(urls: List[str]) -> List[str]:
+    broken_urls = []
+    for index, url in enumerate(urls):
+        if "mailto" not in url:
+            print(f"Checking {index} of {len(urls)} urls", end="\r", flush=True)
+            r = requests.head(url)
+            if r.status_code == 404 or r.status_code == 503:
+                broken_urls.append(url)
+    return broken_urls
