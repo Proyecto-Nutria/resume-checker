9d0a55f1af01f32f539417b68b0db1288160ee36
Gober <reyesfragosoroberto@gmail.com>
2020-08-28 05:41:11 +0000

    Fixe typo in test_network

--- test/test_network.py
+++ test/test_network.py
@@ -1,8 +1,11 @@
+from unittest.mock import patch
+
 from resume_checker.network import _url_checker
 
 url_and_email = ["http://www.google.com", "mailto:nutria@gmial.com"]
 urls_200 = ["http://www.google.com", "http://www.twitter.com"]
-urls_404 = ["https://www.google.com/secrets"]
+urls_404 = ["https://www.sergio-bot.com/"]
+urls_503 = ["https://www.manuel-bot.com/"]
 
 
 def test_no_urls():
@@ -10,12 +13,24 @@ def test_no_urls():
 
 
 def test_url_and_mail():
-    assert _url_checker(url_and_email) == []
-
-
-def test_404_page():
-    assert _url_checker(urls_404) == urls_404
+    with patch("requests.head") as mock_request:
+        mock_request.return_value.status_code = 200
+        assert _url_checker(url_and_email) == []
 
 
 def test_200_pages():
-    assert _url_checker(urls_200) == []
+    with patch("requests.head") as mock_request:
+        mock_request.return_value.status_code = 200
+        assert _url_checker(urls_200) == []
+
+
+def test_404_page():
+    with patch("requests.head") as mock_request:
+        mock_request.return_value.status_code = 404
+        assert _url_checker(urls_404) == urls_404
+
+
+def test_503_page():
+    with patch("requests.head") as mock_request:
+        mock_request.return_value.status_code = 503
+        assert _url_checker(urls_503) == urls_503
